472c34339e13b21d8a8248e5789ea52aa9072fa7
= <=>
2021-03-28 14:42:32 +0700

    PWL-07 : Setting nilai $fillable di Models

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -7,5 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    use HasFactory;
+    protected $table='mahasiswas'; //Eloquent akan membuat model Mahasiswa menyimpan record ditabel mahasiswas
+    public $timestamps= false;
+    protected $primaryKey = 'nim'; // Memanggil isi DB dengan primaryKey
+    /**
+     * The attributes that are mass assignable
+     *  @var array
+     */
+    protected $fillable= [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+        'No_Handphone',
+    ];
 }
